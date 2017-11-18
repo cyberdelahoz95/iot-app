c049d2e837a617f62f4750b37a9ce7bdc4b719f4
Henry de la Hoz <cyberdelahoz95@gmail.com>
2017-11-18 15:03:31 -0500

    Implementing Agent Service

--- iot-app-db/tests/agent-tests.js
+++ iot-app-db/tests/agent-tests.js
@@ -3,19 +3,20 @@
 const test = require('ava')
 const proxyquire = require('proxyquire')
 const sinon = require('sinon')
+
+const agentFixtures = require('./fixtures/agent')
+
 let sandbox = null
-
 let db = null
-
 let MetricStub = {
   belongsTo: sinon.spy() // sinon.spy nos permite tener un registro de las veces que la funcion fue llamada de tal forma que se pueda comprobar que se invoco el metodo que implementa la funcion
 }
-
+let single = Object.assign({}, agentFixtures.single)
 let AgentStub = null
-
 let config = {
   logging: function () {} // deshabilita el logging a la db
 }
+let id = 1
 
 test.beforeEach(async () => {
   sandbox = sinon.sandbox.create() // un sandbox nos permite guardar registros de invocaciones echas a sinon en un ambiente local, es decir especificamente para la prueba que se este ejecutando, esto tomando en cuenta que se ejecutaran multiples test de manera paralela y todos ellos llamaran al beforeeach
@@ -43,4 +44,10 @@ test.serial('Setup', t => {
   t.true(AgentStub.hasMany.calledWith(MetricStub), 'Argument should be Metric Model')
   t.true(MetricStub.belongsTo.called, 'MetricModel.belongsTo was executed')
   t.true(MetricStub.belongsTo.calledWith(AgentStub), 'Argument should be Agent Model')
+})
+
+test.serial('Agent#findById', async t => {
+  let agent = await db.Agent.findById(id)
+
+  t.deepEqual(agent, agentFixtures.byId(id), 'Should be the same')
 })
