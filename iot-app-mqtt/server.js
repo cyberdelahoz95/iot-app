cdb3365a55eac3882ea6a83d13c8b3c738a61dea
Henry <cyberdelahoz95@gmail.com>
2017-11-24 08:41:42 -0500

    Implementing MQTT server and testing with a temporal terminal client

--- iot-app-mqtt/server.js
+++ iot-app-mqtt/server.js
@@ -18,7 +18,16 @@ const settings = {
 
 const server = new mosca.Server(settings)
 
+server.on('clientConnected', (client) => {
+    console.log(`${chalk.magenta('Client Connected')} ${client.id}`)        
+})
+server.on('clientDisconnected', (client) => {
+    console.log(`${chalk.magenta('Client Disconnected')} ${client.id}`)            
+})
+server.on('published', (packet, client) => {
+    console.log(`${chalk.magenta('Received:')} ${packet.topic}`)             
+    debug(`Payload: ${packet.payload}`)   
+})
 server.on('ready', () => {
-    console.log(`${chalk.green('[Iot App MQTT]')} server is running`)
-    
+    console.log(`${chalk.green('[Iot App MQTT]')} server is running`)    
 } )
